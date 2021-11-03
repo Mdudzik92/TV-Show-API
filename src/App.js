9d72b343c3c92cc1ac378c6da34953a71cd7af0a
Matt Dudzik <mdudzik92@gmail.com>
2021-11-03 02:25:11 -0500

    Search component added in filtering the Character Grid by targeting each new input and using onChange to put whatever the input is into the new piece of state

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import Header from './components/ui/Header'
 import CharacterGrid from './components/characters/CharacterGrid'
-import './App.css';
+import Search from './components/ui/Search'
+import './App.css'
 
 const App = () => {
   const [items, setItems] = useState([])
   const [isLoading, setIsLoading] = useState(true)
+  const [query, setQuery] = useState('')
 
   useEffect(() => {
     const fetchItems = async () => {
-      const result = await axios(`https://www.breakingbadapi.com/api/characters`)
+      setIsLoading(true)
+      const result = await axios(
+        `https://www.breakingbadapi.com/api/characters?name=${query}`
+      )
 
       console.log(result.data)
 
@@ -19,14 +24,15 @@ const App = () => {
     }
 
     fetchItems()
-  }, [])
+  }, [query])
 
   return (
-    <div className="container">
+    <div className='container'>
       <Header />
-      <CharacterGrid isLoading={isLoading} items={items}/>
+      <Search getQuery={(q) => setQuery(q)} />
+      <CharacterGrid isLoading={isLoading} items={items} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/ui/Search.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+
+// Every input in the form is going to have its own piece of state
+const Search = ({ getQuery }) => {
+  const [text, setText] = useState('')
+
+  const onChange = (q) => {
+    setText(q)
+    getQuery(q)
+  }
+
+  return (
+    <section className='search'>
+      <form>
+        <input
+          type='text'
+          className='form-control'
+          placeholder='Search characters'
+          value={text}
+          onChange={(e) => onChange(e.target.value)}
+          autoFocus
+        />
+      </form>
+    </section>
+  )
+}
+
+export default Search
